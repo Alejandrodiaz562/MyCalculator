3c09dd7d8b209b1b69bf4e723899cfc821f1f9ca
Alejandrodiaz562 <111014821+Alejandrodiaz562@users.noreply.github.com>
2025-01-15 14:09:12 -0500

    updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,37 @@ const Button = ({value, setOperation, operation, className}) => {
 
     const handleClick=(e)=>{
 
-       console.log(e.target.value)
+        const target = e.target.value
+        const last = operation.slice(-1)
+        const operators = ["/", "*", "+", "-"]
+
+        if (target === "Limpiar"){  
+            setOperation("")
+        } else if (target === "Borrar"){                   
+            setOperation(operation.slice(0, -1))
+        } else if(target === "="){
+            try {
+                setOperation(eval(operation).toString())
+            } catch (error) {
+                return
+            }                            
+           
+        } else {
+            if(operation.length === 0 && operators.includes(target)){
+                return
+            } else{
+                result(target)
+                if(operation === "0" && !operators.includes(target)){
+                    setOperation(target)
+                }
+                if(operators.includes(last)){
+                    if(operators.includes(target)){
+                        const newOperation = operation.slice(0, -1)
+                        setOperation(newOperation + target)
+                    }
+                }
+            }
+        }        
     }
     return ( 
         <button className={className} value={value} onClick={handleClick}>{value}</button>
